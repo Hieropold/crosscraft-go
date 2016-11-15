6e2c090ad19d3ff4b8ca6d0cf4dcd55c74bf9a19
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2016-11-15 16:24:05 +0300

    Basic Go http server.

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/welcome.html");
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	p := &Page{Title: "Welcome"}
+	err = t.Execute(w, p);
+	if (err != nil) {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Crosscraft server starting")
+
+	http.HandleFunc("/", indexHandler)
+
+	fmt.Println("Crosscraft server is listening on port 8080")
+
+	http.ListenAndServe(":8080", nil)
 }
