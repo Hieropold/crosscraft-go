43bf0e7642d53c08f9e158867c357fc9240fb43b
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2017-01-13 14:59:32 +0300

    Added static assets serving.

--- main.go
+++ main.go
@@ -14,18 +14,25 @@ type Page struct {
 var templates = template.Must(template.ParseFiles("templates/main.html", "templates/welcome.html"))
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	//var p = &Page{Title: "Welcome"}
-	var err = templates.ExecuteTemplate(w, "main", nil);
+	err := templates.ExecuteTemplate(w, "main", nil);
 	if (err != nil) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
+func quizHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("quiz!!!");
+}
+
 func main() {
 	fmt.Println("Crosscraft server starting")
 
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/quiz", quizHandler)
+
+	// Static assets
+	http.Handle("/public", http.FileServer(http.Dir("./public/")))
 
 	fmt.Println("Crosscraft server is listening on port 8080")
 
